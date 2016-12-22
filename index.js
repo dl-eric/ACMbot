72a301179aee62981a03afad6fa0e00b0758d935
dl-eric <orangepamelo@gmail.com>
2016-12-22 16:35:57 -0500

    Try Wit

--- index.js
+++ index.js
@@ -128,6 +128,33 @@ function sendTextMessage(sender, text) {
 }
 
 // ----------------------------------------------------------------------------
+// Messenger API specific code
+// See the Send API reference
+// httpz://developers.facebook.com/docs/messenger-platform/send-api-reference
+
+const fbMessage = (id, text) => {
+  const body = JSON.stringify({
+    recipient: { id },
+    message: { text },
+  });
+
+  const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
+  return fetch('https://graph.facebook.com/me/messages?' + qs, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body,
+  })
+  .then(rsp => rsp.json())
+  .then(json => {
+    if (json.error && json.error.message) {
+      throw new Error(json.error.message);
+    }
+
+    return json;
+  });
+};
+
+// ----------------------------------------------------------------------------
 // Wit.ai bot specific code
 
 // This will contain all user sessions.
