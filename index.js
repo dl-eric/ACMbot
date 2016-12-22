7a4e468e7b32e76e432cee75d5df4fc6e90e880e
dl-eric <orangepamelo@gmail.com>
2016-12-22 15:22:25 -0500

    Add template functionality

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('This is not the page you\'re looking for...')
 })
 
 // for Facebook verification
@@ -30,3 +30,38 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+// API Endpoint
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAT6by7HlrMBAOHgKcSI1QW9PZBUZAtYvuZCUvVphDCKRYYvC08lgOVziZBZBqmZB5uHwzxIANffnZCRH5In98yQovweolMaUgneZBDS9BD7p2jmN66SYQj0DKP9mjeUslq7BfXXGZAcDIYbjFq0cho1ZAfHqgZANynUA0UZAn3q2wzivgZDZD"
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
