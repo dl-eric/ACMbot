f493347b252aef29182a35095086e0330e7ab89b
dl-eric <orangepamelo@gmail.com>
2016-12-23 18:15:31 -0500

    Fix crash on wit token.

--- services/wit.js
+++ services/wit.js
@@ -105,7 +105,7 @@ var actions = {
 // SETUP THE WIT.AI SERVICE
 var getWit = function () {
 	console.log('GRABBING WIT')
-	return new Wit(Config.WIT_TOKEN, actions)
+	return new Wit('PRVERNRQW4KI4KUOTHI43QT2SH6U2IBQ', actions)
 }
 
 module.exports = {
