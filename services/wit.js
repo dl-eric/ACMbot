36ca7da3118e5eb130671d05b8eeea88843fa73f
dl-eric <orangepamelo@gmail.com>
2016-12-23 18:54:40 -0500

    Trying to rigorously debug.

--- services/wit.js
+++ services/wit.js
@@ -2,7 +2,7 @@
 
 var Config = require('../config')
 var FB = require('../connectors/facebook')
-var Wit = require('../').Wit
+var Wit = require('node-wit').Wit
 var request = require('request')
 
 
@@ -105,7 +105,7 @@ var actions = {
 // SETUP THE WIT.AI SERVICE
 var getWit = function () {
 	console.log('GRABBING WIT')
-	return new Wit(Config.WIT_TOKEN, actions)
+	return new Wit({accessToken: Config.WIT_TOKEN, actions})
 }
 
 module.exports = {
