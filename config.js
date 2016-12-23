4591e5bf8448a4cf7802cf57f6d025dd53f93eab
dl-eric <orangepamelo@gmail.com>
2016-12-23 18:39:53 -0500

    Trying to rigorously debug.

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const WIT_TOKEN = process.env.WIT_TOKEN;
 const FB_PAGE_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 
-
+console.log(WIT_TOKEN);
 if (!WIT_TOKEN) {
     throw new Error('Missing WIT_TOKEN.');
 }
