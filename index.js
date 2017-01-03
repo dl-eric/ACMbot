3f9e68c998cb75b882035aef34c3c235e956fbb7
dl-eric <orangepamelo@gmail.com>
2017-01-03 14:05:45 -0500

    Try to fix newMessage

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const PORT = process.env.PORT || 8445;
 const WIT_TOKEN = process.env.WIT_TOKEN;
 
 // Messenger API parameters
-const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+const FB_PAGE_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
 if (!FB_PAGE_TOKEN) { throw new Error('missing FB_PAGE_TOKEN') }
 const FB_APP_SECRET = process.env.FB_APP_SECRET;
 if (!FB_APP_SECRET) { throw new Error('missing FB_APP_SECRET') }
