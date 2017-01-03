8d60d6e9a594ebdf62366e2f395c15e16b8d13bb
dl-eric <orangepamelo@gmail.com>
2017-01-03 14:15:00 -0500

    W0t

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/webhook/', (req, res) => {
 
           // We retrieve the user's current session, or create one if it doesn't exist
           // This is needed for our bot to figure out the conversation history
-          const sessionId = findOrCreateSession(sender);
+          const sessionId = Bot.findOrCreateSession(sender);
 
           // We retrieve the message content
           const {text, attachments} = event.message;
