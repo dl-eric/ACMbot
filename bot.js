5cfa958edd25c7c0d29761943abe8c671b33417c
dl-eric <orangepamelo@gmail.com>
2017-01-03 15:42:07 -0500

    JavaScript is ridiculous.

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var request = require('request')
 
 // WIT ACTIONS
 var actions = {
-    send({sessionId}, {text}) {
+    send(sessionId, text) {
         // Our bot has something to say!
         // Let's retrieve the Facebook user whose session belongs to
         const recipientId = sessions[sessionId].fbid;
--- connectors/facebook.js
+++ connectors/facebook.js
@@ -13,7 +13,6 @@ var newMessage = function (recipientId, msg) {
 	console.log(body)
   
   	const qs = 'access_token=' + Config.FB_PAGE_TOKEN;
-	console.log(qs)
   	return fetch('https://graph.facebook.com/v2.6/me/messages?' + qs, {
     	method: 'POST',
     	headers: {'Content-Type': 'application/json'},
