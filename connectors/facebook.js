01414fd9dd43a728adf35c958f44f7748152161e
dl-eric <orangepamelo@gmail.com>
2017-01-03 22:12:57 -0500

    Delete unused code.

--- connectors/facebook.js
+++ connectors/facebook.js
@@ -3,7 +3,7 @@
 var request = require('request')
 var Config = require('../config')
 
-// SETUP A MESSAGE FOR THE FACEBOOK REQUEST
+// POST to Facebook
 var newMessage = function (id, text) {
 	const body = JSON.stringify({
     	recipient: {id},
@@ -26,22 +26,6 @@ var newMessage = function (id, text) {
   	});
 }
 
-// PARSE A FACEBOOK MESSAGE to get user, message body, or attachment
-// https://developers.facebook.com/docs/messenger-platform/webhook-reference
-var getMessageEntry = function (body) {
-	var val = body.object === 'page' &&
-						body.entry &&
-						Array.isArray(body.entry) &&
-						body.entry.length > 0 &&
-						body.entry[0] &&
-						body.entry[0].messaging &&
-						Array.isArray(body.entry[0].messaging) &&
-						body.entry[0].messaging.length > 0 &&
-						body.entry[0].messaging[0]
-	return val || null
-}
-
 module.exports = {
 	newMessage: newMessage,
-	getMessageEntry: getMessageEntry,
 }
