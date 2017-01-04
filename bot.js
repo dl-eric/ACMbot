61d0077898455f742eb8af5c4a435b69cd36a11f
Minhyuk Park <minpark815@gmail.com>
2017-01-05 00:41:50 +0900

    Fix broken files and add necessary lines for command line testing

--- bot.js
+++ bot.js
@@ -4,10 +4,15 @@ const Config = require('./config') // these could all be consts
 const FB = require('./connectors/facebook')
 const Wit = require('node-wit').Wit
 // request is no longer needed
+const {interactive} = require('node-wit'); // this is here for testing in command line
 
 // WIT ACTIONS
 const actions = {
     send({sessionId}, {text}) {
+		if (require.main === module) {
+			console.log(text);
+		      return;
+		    }
         // Our bot has something to say!
         // Let's retrieve the Facebook user whose session belongs to
         const recipientId = sessions[sessionId].fbid;
@@ -40,7 +45,12 @@ const getWit = function () {
 
 var myWit = getWit()
 
-module.exports = {
-	findOrCreateSession: findOrCreateSession,
-	read: read
+
+// bot testing mode
+// use this to test the bot in command line without deploying
+// the command is WIT_TOKEN = {wit token} node bot
+if (require.main === module) {
+  console.log("Bot testing mode.");
+  const client = getWit();
+  interactive(client);
 }
--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const WIT_TOKEN = process.env.WIT_TOKEN;
-const FB_PAGE_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
-const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+var FB_PAGE_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
+var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 
 if (!WIT_TOKEN) {
     throw new Error('Missing WIT_TOKEN.');
@@ -10,10 +10,12 @@ if (!WIT_TOKEN) {
 
 if (!FB_PAGE_TOKEN) {
     throw new Error('Missing FB_PAGE_TOKEN');
+	// FB_PAGE_TOKEN = "uncomment for testing";
 }
 
 if (!FB_VERIFY_TOKEN) {
     throw new Error('Missing FB_VERIFY_TOKEN');
+	// FB_VERIFY_TOKEN = "uncomment for testing";
 }
 
 module.exports = {
