22a68ad49acef1f83fd969e1b0b358ffec3742ed
dl-eric <orangepamelo@gmail.com>
2017-01-03 22:33:50 -0500

    Add FB API code.

--- connectors/facebook.js
+++ connectors/facebook.js
@@ -3,7 +3,7 @@
 var request = require('request')
 var Config = require('../config')
 
-// POST to Facebook
+// SETUP A MESSAGE FOR THE FACEBOOK REQUEST
 var newMessage = function (id, text) {
 	const body = JSON.stringify({
     	recipient: {id},
@@ -24,6 +24,21 @@ var newMessage = function (id, text) {
     	
 		return json;
   	});
+}
+
+// PARSE A FACEBOOK MESSAGE to get user, message body, or attachment
+// https://developers.facebook.com/docs/messenger-platform/webhook-reference
+var getMessageEntry = function (body) {
+	var val = body.object === 'page' &&
+						body.entry &&
+						Array.isArray(body.entry) &&
+						body.entry.length > 0 &&
+						body.entry[0] &&
+						body.entry[0].messaging &&
+						Array.isArray(body.entry[0].messaging) &&
+						body.entry[0].messaging.length > 0 &&
+						body.entry[0].messaging[0]
+	return val || null
 }
 
 module.exports = {
